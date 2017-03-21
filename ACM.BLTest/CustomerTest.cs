b27dddd4d6aa975b28fc4d0d6bb6f1453808f100
TTLWIN2K\partha <parth.aggarwal@thetrainline.com>
2017-03-21 19:02:02 +0000

    Practice Material

---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid() 
+        {
+            //-- Arrange
+            Customer customer = new Customer();
+            customer.FirstName ="Bilbo";
+            customer.LastName = "Baggins";
+            string expected = "Baggins, Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer();
+            customer.LastName = "Baggins";
+            string expected = "Baggins";
+
+            //--Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            string expected = "Bilbo";
+
+            //--Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            //-- Arrange
+            var c1 = new Customer();
+            c1.FirstName = "Bilbo";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Frodo";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Rosie";
+            Customer.InstanceCount += 1;
+
+            //-- Act
+
+            int expected = 3;
+
+
+            //-- Assert
+
+            Assert.AreEqual(expected, Customer.InstanceCount);
+        }
+
+
+    }
+}
---
+++ ClassLibrary1/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public static int InstanceCount { get; set; }
+
+        private string _lastname;
+
+        public string LastName
+        {
+            get
+            {
+                return _lastname;
+            }
+            set
+            {
+                _lastname = value;
+            }
+        }
+
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+        public int CustomerId { get; private set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrEmpty(LastName))
+                    {
+                        fullName += ", ";
+                    }
+
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+
+    }
+}
